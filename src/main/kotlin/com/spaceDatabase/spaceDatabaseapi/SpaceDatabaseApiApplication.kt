37d0b7f7e396cc9360f304d2b6d53472775d779f
Jacob Francois <jafranswa@gmail.com>
2021-12-18 14:25:11 -0700

    initial commit

---
+++ src/main/kotlin/com/spaceDatabase/spaceDatabaseapi/SpaceDatabaseApiApplication.kt
@@ -0,0 +1,11 @@
+package com.spaceDatabase.spaceDatabaseapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpaceDatabaseApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpaceDatabaseApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/spaceDatabase/spaceDatabaseapi/SpaceDatabaseApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spaceDatabase.spaceDatabaseapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpaceDatabaseApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
